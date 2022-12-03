4fe29e68c31ac52df65f655fd4fbe8a675c6539a
mary <macbookpro@macbooks-MacBook-Pro.local>
2022-12-03 19:17:37 +0700

    web monstu

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.hamburger').on('click', function() {
+        $('.nav-grid').toggleClass("open");
+    });
+});
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
